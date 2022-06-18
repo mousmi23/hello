47961cb498d76c3c7e19135c8ddf32ca206cbe11
Mousmi <mousumi23rd.aug@gmail.com>
2022-06-18 18:51:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext } from "react";
+import CompA from "./CompA";
+import "./App.css";
 
-function App() {
+const firstName = createContext();
+const lastName = createContext();
+const App = () => {
+  // Counter App-------------------------------
+  //   const [myNum, setmyNum] = useState(0);
+
+  //   const changeNum1 = () => {
+  //     setmyNum(myNum + 1);
+  //   };
+
+  //   const changeNum2 = () => {
+  //     setmyNum(myNum - 1);
+  //   };
+  //   const reset = () => {
+  //     setmyNum(0);
+  //   };
+
+  //   return (
+  //     <div>
+  //       <h1>{myNum}</h1>
+  //       <button className="btn" onClick={changeNum1}>
+  //         Increase
+  //       </button>
+  //       <button className="btn" onClick={changeNum2}>
+  //         Decrease
+  //       </button>
+  //       <button className="btn" onClick={reset}>
+  //         Reset
+  //       </button>
+  //     </div>
+  //   );
+  // };
+
+  // Note - Name Change App---------------------
+
+  // const [myName, setmyName] = useState("Buni");
+
+  // const changeName = () => {
+  //   let val = myName;
+  //   if (val === "Buni") {
+  //     setmyName("Mou");
+  //   } else {
+  //     setmyName("Buni");
+  //   }
+  // };
+
+  // return (
+  //   <div>
+  //     <h1>{myName}</h1>
+  //     <button className="btn" onClick={changeName}>
+  //       Change
+  //     </button>
+  //   </div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <firstName.Provider value={"Mousumi"}>
+        <lastName.Provider value={"kundu"}>
+          <CompA />
+        </lastName.Provider>
+      </firstName.Provider>
+    </>
   );
-}
+};
 
 export default App;
+export { firstName, lastName };
---
+++ src/CompA.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { firstName, lastName } from "./App";
+
+const CompA = () => {
+  return (
+    <>
+      <firstName.Consumer>
+        {(fname) => {
+          return (
+            <lastName.Consumer>
+              {(lname) => {
+                return (
+                  <h1>
+                    My name is {fname} {lname}
+                  </h1>
+                );
+              }}
+            </lastName.Consumer>
+          );
+        }}
+      </firstName.Consumer>
+    </>
+  );
+};
+
+export default CompA;
